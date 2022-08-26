28e02403622ce068f4975453871ad554f4182805
chopinxiao <chopinxiao@tencent.com>
2022-08-26 15:42:39 +0800

    feat: solved Problem No.2206. Divide Array Into Equal Pairs

--- aug.go
+++ aug.go
@@ -606,3 +606,44 @@ func (h *BaseHeap) Pop() (index int, value int) {
 	}
 	return index, value
 }
+
+// Problem No.2206. Divide Array Into Equal Pairs
+//
+// You are given an integer array nums consisting of 2 * n integers.
+// You need to divide nums into n pairs such that:
+// 	Each element belongs to exactly one pair.
+// 	The elements present in a pair are equal.
+// 	Return true if nums can be divided into n pairs, otherwise return false.
+//
+// Example 1:
+//
+// Input: nums = [3,2,3,2,2,2]
+// Output: true
+// Explanation:
+// There are 6 elements in nums, so they should be divided into 6 / 2 = 3 pairs.
+// If nums is divided into the pairs (2, 2), (3, 3), and (2, 2), it will satisfy all the conditions.
+// Example 2:
+//
+// Input: nums = [1,2,3,4]
+// Output: false
+// Explanation:
+// There is no way to divide nums into 4 / 2 = 2 pairs such that the pairs satisfy every condition.
+//
+//
+// Constraints:
+//
+// nums.length == 2 * n
+// 1 <= n <= 500
+// 1 <= nums[i] <= 500
+func divideArray(nums []int) bool {
+	bitMap := make([]int, 501/strconv.IntSize+1)
+	for i := 0; i < len(nums); i++ {
+		bitMap[nums[i]/strconv.IntSize] = bitMap[nums[i]/strconv.IntSize] ^ 1<<(nums[i]%strconv.IntSize)
+	}
+	for i := 0; i < len(bitMap); i++ {
+		if bitMap[i] != 0 {
+			return false
+		}
+	}
+	return true
+}
--- aug_test.go
+++ aug_test.go
@@ -611,3 +611,37 @@ func Test_kthSmallest(t *testing.T) {
 		})
 	}
 }
+
+func Test_divideArray(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "sample",
+			args: args{[]int{1, 2, 3, 4}},
+			want: false,
+		},
+		{
+			name: "sample",
+			args: args{[]int{1, 2, 2, 1, 3, 3}},
+			want: true,
+		},
+		{
+			name: "badCase",
+			args: args{[]int{9, 9, 19, 10, 9, 12, 2, 12, 3, 3, 11, 5, 8, 4, 13, 6, 2, 11, 9, 19, 11, 15, 9, 17, 15, 12, 5, 14, 12, 16, 18, 16, 10, 3, 8, 9, 16, 20, 2, 4, 16, 12, 11, 14, 20, 16, 2, 18, 17, 20, 3, 13, 16, 17, 1, 1, 11, 20, 20, 4}},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := divideArray(tt.args.nums); got != tt.want {
+				t.Errorf("divideArray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
