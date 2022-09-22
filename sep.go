c8dd91bba16435909e390dd82ba63ac20156bae8
chopinxiao <chopinxiao@tencent.com>
2022-09-22 19:55:00 +0800

    feat: Problem No.1898 Maximum Number of Removable Characters

--- sep.go
+++ sep.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"math/rand"
 	"sort"
 )
@@ -355,7 +354,7 @@ func findMinArrowShots2(points [][]int) int {
 		right.Indexs[i] = i
 	}
 	sort.Sort(right)
-	fmt.Printf("rightIndex: %v", right.Indexs)
+	// fmt.Printf("rightIndex: %v", right.Indexs)
 	left := LeftList{
 		Points: points,
 		Indexs: make([]int, len(points)),
@@ -364,7 +363,7 @@ func findMinArrowShots2(points [][]int) int {
 		left.Indexs[i] = i
 	}
 	sort.Sort(left)
-	fmt.Printf("leftIndex: %v", left.Indexs)
+	// fmt.Printf("leftIndex: %v", left.Indexs)
 	visited := make(map[int]int)
 	ans, j := 0, 0
 	for i := 0; i < len(right.Indexs); i++ {
@@ -376,7 +375,7 @@ func findMinArrowShots2(points [][]int) int {
 			visited[left.Indexs[j]] = 0
 			j++
 		}
-		fmt.Printf("visited: %v\n", visited)
+		// fmt.Printf("visited: %v\n", visited)
 		ans++
 		if len(visited) == len(points) {
 			break
@@ -441,7 +440,7 @@ func orderOfLargestPlusSign(n int, mines [][]int) int {
 				c := lastC + maxOrder
 				lastRIndex := findBlock(cols[c], r, 0, len(cols[c])-1)
 				lastR, currR := cols[c][lastRIndex], cols[c][lastRIndex+1]
-				fmt.Printf("(%d, %d), r(%d, %d), c(%d, %d), min=%d\n", r, c, lastR, currR, lastC, currC, min(min(currR-r, r-lastR), maxOrder))
+				// fmt.Printf("(%d, %d), r(%d, %d), c(%d, %d), min=%d\n", r, c, lastR, currR, lastC, currC, min(min(currR-r, r-lastR), maxOrder))
 				if min(min(currR-r, r-lastR), maxOrder) > ans {
 					ans = min(min(currR-r, r-lastR), maxOrder)
 				}
@@ -451,7 +450,7 @@ func orderOfLargestPlusSign(n int, mines [][]int) int {
 				c = currC - maxOrder
 				lastRIndex = findBlock(cols[c], r, 0, len(cols[c])-1)
 				lastR, currR = cols[c][lastRIndex], cols[c][lastRIndex+1]
-				fmt.Printf("(%d, %d), r(%d, %d), c(%d, %d), min=%d\n", r, c, lastR, currR, lastC, currC, min(min(currR-r, r-lastR), maxOrder))
+				// fmt.Printf("(%d, %d), r(%d, %d), c(%d, %d), min=%d\n", r, c, lastR, currR, lastC, currC, min(min(currR-r, r-lastR), maxOrder))
 				if min(min(currR-r, r-lastR), maxOrder) > ans {
 					ans = min(min(currR-r, r-lastR), maxOrder)
 				}
@@ -652,4 +651,79 @@ func wiggleMaxLength2(nums []int) int {
 		return upLen
 	}
 	return downLen
+}
+
+// Problem No.1898 Maximum Number of Removable Characters
+//
+// You are given two strings s and p where p is a subsequence of s. You are also given a distinct 0-indexed integer
+// array removable containing a subset of indices of s (s is also 0-indexed).
+//
+// You want to choose an integer k (0 <= k <= removable.length) such that, after removing k characters from s using the
+// first k indices in removable, p is still a subsequence of s. More formally, you will mark the character at
+// s[removable[i]] for each 0 <= i < k, then remove all marked characters and check if p is still a subsequence.
+//
+// Return the maximum k you can choose such that p is still a subsequence of s after the removals.
+//
+// A subsequence of a string is a new string generated from the original string with some characters (can be none)
+// deleted without changing the relative order of the remaining characters.
+//
+// Example 1:
+// 		Input: s = "abcacb", p = "ab", removable = [3,1,0]
+// 		Output: 2
+// 		Explanation: After removing the characters at indices 3 and 1, "abcacb" becomes "accb".
+// 		"ab" is a subsequence of "accb".
+// 		If we remove the characters at indices 3, 1, and 0, "abcacb" becomes "ccb", and "ab" is no longer a subsequence.
+// 		Hence, the maximum k is 2.
+//
+// Example 2:
+// 		Input: s = "abcbddddd", p = "abcd", removable = [3,2,1,4,5,6]
+// 		Output: 1
+// 		Explanation: After removing the character at index 3, "abcbddddd" becomes "abcddddd".
+// 		"abcd" is a subsequence of "abcddddd".
+//
+// Example 3:
+// 		Input: s = "abcab", p = "abc", removable = [0,1,2,3,4]
+// 		Output: 0
+// 		Explanation: If you remove the first index in the array removable, "abc" is no longer a subsequence.
+//
+// Constraints:
+// 		1 <= p.length <= s.length <= 105
+// 		0 <= removable.length < s.length
+// 		0 <= removable[i] < s.length
+// 		p is a subsequence of s.
+// 		s and p both consist of lowercase English letters.
+// 		The elements in removable are distinct.
+func maximumRemovals(s string, p string, removable []int) int {
+	mask := make([]int, len(s))
+	for i := 0; i < len(removable); i++ {
+		mask[removable[i]] = i + 1
+	}
+	start, end := 0, len(removable)
+	for start < end {
+		mid := (start + end) / 2
+		if isSubs(s, p, mask, mid) {
+			if start == mid {
+				if isSubs(s, p, mask, end) {
+					start++
+				}
+				break
+			}
+			start = mid
+		} else {
+			end = mid - 1
+		}
+	}
+	return start
+}
+
+func isSubs(s string, p string, mask []int, limit int) bool {
+	for i, j := 0, 0; i < len(s); i++ {
+		if s[i] == p[j] && (mask[i] == 0 || mask[i] > limit) {
+			j++
+			if j >= len(p) {
+				return true
+			}
+		}
+	}
+	return false
 }
