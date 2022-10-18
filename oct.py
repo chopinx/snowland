b5ad50f9ad4888d12b2fd67213cced9534a74e4e
chopinxiao <chopinxiao@tencent.com>
2022-10-18 11:07:39 +0800

    feat: solved Problem No.12 Integer to Roman.

--- oct.py
+++ oct.py
@@ -53,3 +53,61 @@ class Solution:
             path[v2] = False
         visited[v] = True
         return False
+
+    # Problem No.12 Integer to Roman.
+    #
+    # Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+    #   Symbol       Value
+    #   I             1
+    #   V             5
+    #   X             10
+    #   L             50
+    #   C             100
+    #   D             500
+    #   M             1000
+    # For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is
+    # simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+    #
+    # Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not
+    # IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four.
+    # The same principle applies to the number nine, which is written as IX. There are six instances where subtraction
+    # is used:
+    #   I can be placed before V (5) and X (10) to make 4 and 9.
+    #   X can be placed before L (50) and C (100) to make 40 and 90.
+    #   C can be placed before D (500) and M (1000) to make 400 and 900.
+    # Given an integer, convert it to a roman numeral.
+    #
+    # Example 1:
+    #   Input: num = 3
+    #   Output: "III"
+    #   Explanation: 3 is represented as 3 ones.
+    #
+    # Example 2:
+    #   Input: num = 58
+    #   Output: "LVIII"
+    #   Explanation: L = 50, V = 5, III = 3.
+    #
+    # Example 3:
+    #   Input: num = 1994
+    #   Output: "MCMXCIV"
+    #   Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+    #
+    # Constraints:
+    #   1 <= num <= 3999
+    def intToRoman(self, num: int) -> str:
+        symbols, halfs, i = "IXCM", "VLD", -1
+        ans = ""
+        while num > 0:
+            d = num % 10
+            num, i = num // 10, i + 1
+            if d == 0:
+                continue
+            if d < 4:
+                ans = symbols[i] * d + ans
+            elif d == 4:
+                ans = symbols[i] + halfs[i] + ans
+            elif d < 9:
+                ans = halfs[i] + symbols[i] * (d - 5) + ans
+            elif d == 9:
+                ans = symbols[i] + symbols[i + 1] + ans
+        return ans
--- test_oct.py
+++ test_oct.py
@@ -16,3 +16,17 @@ class TestSolution(TestCase):
         for i in range(len(cases)):
             name, args, wants = cases[i]
             self.assertEqual(wants, oct.Solution().canFinish(*args), "[%d]name=%s" % (i, name))
+
+    def test_intToRoman(self):
+        cases = [
+            # name, args, wants
+            ("sample", (2,), "II"),
+            ("sample", (4,), "IV"),
+            ("sample", (9,), "IX"),
+            ("sample", (27,), "XXVII"),
+            ("sample", (58,), "LVIII"),
+            ("sample", (1994,), "MCMXCIV"),
+        ]
+        for i in range(len(cases)):
+            name, args, wants = cases[i]
+            self.assertEqual(wants, oct.Solution().intToRoman(*args), "[%d]name=%s" % (i, name))
