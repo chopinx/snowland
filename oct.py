cafd1e2a6d7f4dcdd52c54cf5b8ee7f0339e4dfa
chopinxiao <chopinxiao@tencent.com>
2022-10-19 16:02:09 +0800

    feat: solved Problem No.253 Meeting Rooms II

--- oct.py
+++ oct.py
@@ -54,6 +54,66 @@ class Solution:
         visited[v] = True
         return False
 
+    # Problem No.210 Course Schedule II
+    #
+    # There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an
+    # array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to
+    # take course ai.
+    #
+    # For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+    # Return the ordering of courses you should take to finish all courses. If there are many valid answers, return any
+    # of them. If it is impossible to finish all courses, return an empty array.
+    #
+    # Example 1:
+    #   Input: numCourses = 2, prerequisites = [[1,0]]
+    #   Output: [0,1]
+    #   Explanation: There are a total of 2 courses to take. To take course 1 you should have finished course 0. So the
+    #   correct course order is [0,1].
+    #
+    # Example 2:
+    #   Input: numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]]
+    #   Output: [0,2,1,3]
+    #   Explanation: There are a total of 4 courses to take. To take course 3 you should have finished both courses 1
+    #   and 2. Both courses 1 and 2 should be taken after you finished course 0.
+    #   So one correct course order is [0,1,2,3]. Another correct ordering is [0,2,1,3].
+    #
+    # Example 3:
+    #   Input: numCourses = 1, prerequisites = []
+    #   Output: [0]
+    #
+    # Constraints:
+    #   1 <= numCourses <= 2000
+    #   0 <= prerequisites.length <= numCourses * (numCourses - 1)
+    #   prerequisites[i].length == 2
+    #   0 <= ai, bi < numCourses
+    #   ai != bi
+    #   All the pairs [ai, bi] are distinct.
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        neighbors = [[] for _ in range(numCourses)]
+        for e in prerequisites:
+            neighbors[e[0]].append(e[1])
+        visited = [False for _ in range(numCourses)]
+        path = [False for _ in range(numCourses)]
+        top_list = []
+        for v in range(numCourses):
+            if not visited[v] and self.isCycle2(v, neighbors, visited, path, top_list):
+                return []
+        return top_list
+
+    def isCycle2(self, v: int, neighbors: list, visited: list, path: list, top_list: list):
+        for v2 in neighbors[v]:
+            if visited[v2]:
+                continue
+            if path[v2]:
+                return True
+            path[v2] = True
+            if self.isCycle2(v2, neighbors, visited, path, top_list):
+                return True
+            path[v2] = False
+        visited[v] = True
+        top_list.append(v)
+        return False
+
     # Problem No.12 Integer to Roman.
     #
     # Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
@@ -110,4 +170,31 @@ class Solution:
                 ans = halfs[i] + symbols[i] * (d - 5) + ans
             elif d == 9:
                 ans = symbols[i] + symbols[i + 1] + ans
+        return ans
+
+    # Problem No.253 Meeting Rooms II
+    #
+    # Given an array of meeting time intervals intervals where intervals[i] = [starti, endi], return the minimum number
+    # of conference rooms required.
+    #
+    # Example 1:
+    #   Input: intervals = [[0,30],[5,10],[15,20]]
+    #   Output: 2
+    #
+    # Example 2:
+    #   Input: intervals = [[7,10],[2,4]]
+    #   Output: 1
+    #
+    # Constraints:
+    #   1 <= intervals.length <= 10^4
+    #   0 <= starti < endi <= 10^6
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        starts = sorted([i[0] for i in intervals])
+        ends = sorted([i[1] for i in intervals])
+        ans, ep = 0, 0
+        for t in starts:
+            if t >= ends[ep]:
+                ans -= 1
+                ep += 1
+            ans += 1
         return ans
