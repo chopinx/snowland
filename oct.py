851f379f2b679ca83a3360d32432fbdcecc1148d
chopinxiao <chopinxiao@tencent.com>
2022-10-21 12:32:10 +0800

    feat: solved Problem No.295 Find Median from Data Stream

--- oct.py
+++ oct.py
@@ -1,3 +1,4 @@
+import heapq
 from typing import List
 
 
@@ -46,6 +47,85 @@ class ParkingSystem:
             return True
         return False
 
+
+# Problem No.295 Find Median from Data Stream
+#
+# The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value
+# and the median is the mean of the two middle values.
+#
+# For example, for arr = [2,3,4], the median is 3.
+# For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
+# Implement the MedianFinder class:
+#
+# MedianFinder() initializes the MedianFinder object.
+# void addNum(int num) adds the integer num from the data stream to the data structure.
+# double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be
+# accepted.
+#
+# Example 1:
+#   Input
+#   ["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
+#   [[], [1], [2], [], [3], []]
+#   Output
+#   [null, null, null, 1.5, null, 2.0]
+#
+#   Explanation
+#   MedianFinder medianFinder = new MedianFinder();
+#   medianFinder.addNum(1);    // arr = [1]
+#   medianFinder.addNum(2);    // arr = [1, 2]
+#   medianFinder.findMedian(); // return 1.5 (i.e., (1 + 2) / 2)
+#   medianFinder.addNum(3);    // arr[1, 2, 3]
+#   medianFinder.findMedian(); // return 2.0
+#
+# Constraints:
+#   -10^5 <= num <= 10^5
+#   There will be at least one element in the data structure before calling findMedian.
+#   At most 5 * 10^4 calls will be made to addNum and findMedian.
+#
+#
+# Follow up:
+#   If all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
+#   If 99% of all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
+class MedianFinder:
+    def __init__(self):
+        self.low_list = []
+        self.high_list = []
+
+    def _push_high(self, num: int):
+        heapq.heappush(self.high_list, num)
+
+    def _push_low(self, num: int):
+        heapq.heappush(self.low_list, 0 - num)
+
+    def _pop_high(self) -> int:
+        return heapq.heappop(self.high_list)
+
+    def _pop_low(self) -> int:
+        return 0 - heapq.heappop(self.low_list)
+
+    def addNum(self, num: int) -> None:
+        if len(self.high_list) == 0 and len(self.low_list) == 0:
+            self._push_high(num)
+            return
+        if num > self.high_list[0]:
+            self._push_high(num)
+        else:
+            self._push_low(num)
+        if len(self.high_list) > len(self.low_list) + 1:
+            self._push_low(self._pop_high())
+        elif len(self.low_list) > len(self.high_list):
+            self._push_high(self._pop_low())
+
+    def findMedian(self) -> float:
+        if len(self.high_list) > len(self.low_list):
+            return self.high_list[0]
+        return (self.high_list[0] - self.low_list[0]) / 2
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
 
 class Solution:
     # Problem No.207 Course Schedule
