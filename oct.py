bff69d2a6d771d8a2034ba3a3ff73d5d352132b8
chopinxiao <chopinxiao@tencent.com>
2022-10-26 10:25:41 +0800

    feat: solved Problem No.588 Design a data structure that simulates an in-memory file system.

--- oct.py
+++ oct.py
@@ -2,6 +2,103 @@ import heapq
 from typing import List, Optional
 
 
+# Problem No.588 Design a data structure that simulates an in-memory file system.
+#
+# Implement the FileSystem class:
+#
+# FileSystem() Initializes the object of the system.
+#
+# List<String> ls(String path)
+# If path is a file path, returns a list that only contains this file's name.
+# If path is a directory path, returns the list of file and directory names in this directory.
+# The answer should in lexicographic order.
+#
+# void mkdir(String path) Makes a new directory according to the given path. The given directory path does not exist.
+# If the middle directories in the path do not exist, you should create them as well.
+#
+# void addContentToFile(String filePath, String content)
+# If filePath does not exist, creates that file containing given content.
+# If filePath already exists, appends the given content to original content.
+#
+# String readContentFromFile(String filePath) Returns the content in the file at filePath.
+#
+# Example 1:
+#   Input
+#   ["FileSystem", "ls", "mkdir", "addContentToFile", "ls", "readContentFromFile"]
+#   [[], ["/"], ["/a/b/c"], ["/a/b/c/d", "hello"], ["/"], ["/a/b/c/d"]]
+#   Output
+#   [null, [], null, null, ["a"], "hello"]
+#
+#   Explanation
+#   FileSystem fileSystem = new FileSystem();
+#   fileSystem.ls("/");                         // return []
+#   fileSystem.mkdir("/a/b/c");
+#   fileSystem.addContentToFile("/a/b/c/d", "hello");
+#   fileSystem.ls("/");                         // return ["a"]
+#   fileSystem.readContentFromFile("/a/b/c/d"); // return "hello"
+#
+# Constraints:
+#   1 <= path.length, filePath.length <= 100
+#   path and filePath are absolute paths which begin with '/' and do not end with '/' except that the path is just "/".
+#   You can assume that all directory names and file names only contain lowercase letters, and the same names will not exist in the same directory.
+#   You can assume that all operations will be passed valid parameters, and users will not attempt to retrieve file content or list a directory or file that does not exist.
+#   1 <= content.length <= 50
+#   At most 300 calls will be made to ls, mkdir, addContentToFile, and readContentFromFile.
+#
+# Your FileSystem object will be instantiated and called as such:
+# obj = FileSystem()
+# param_1 = obj.ls(path)
+# obj.mkdir(path)
+# obj.addContentToFile(filePath,content)
+# param_4 = obj.readContentFromFile(filePath)
+class FileSystem:
+
+    def __init__(self):
+        self.root = {}
+
+    def _parse_path(self, path: str) -> list:
+        if path == "/":
+            return []
+        return path[1:].split("/")
+
+    def ls(self, path: str) -> List[str]:
+        name_list = self._parse_path(path)
+        curr_dir = self.root
+        for name in name_list:
+            curr_dir = curr_dir[name]
+        if isinstance(curr_dir, str):
+            return [name_list[-1]]
+        return sorted(curr_dir.keys())
+
+    def mkdir(self, path: str) -> None:
+        name_list = self._parse_path(path)
+        curr_dir = self.root
+        for name in name_list:
+            if name in curr_dir:
+                continue
+            curr_dir[name] = {}
+            curr_dir = curr_dir[name]
+
+    def addContentToFile(self, filePath: str, content: str) -> None:
+        name_list = self._parse_path(filePath)
+        curr_dir = self.root
+        for name in name_list[:-1]:
+            curr_dir = curr_dir[name]
+        filename = name_list[-1]
+        if filename not in curr_dir:
+            curr_dir[filename] = content
+        else:
+            curr_dir[filename] += content
+
+    def readContentFromFile(self, filePath: str) -> str:
+        name_list = self._parse_path(filePath)
+        curr_dir = self.root
+        for name in name_list[:-1]:
+            curr_dir = curr_dir[name]
+        filename = name_list[-1]
+        return curr_dir[filename]
+
+
 # Problem No.1603 Design Parking System
 #
 # Design a parking system for a parking lot. The parking lot has three kinds of parking spaces: big, medium, and small,
