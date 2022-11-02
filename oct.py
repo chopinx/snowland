45189a736bf9eb303f2a647115190b8c426a6575
chopinxiao <chopinxiao@tencent.com>
2022-11-02 14:39:10 +0800

    feat: solved Problem No.909 Snakes and Ladders

--- oct.py
+++ oct.py
@@ -896,18 +896,93 @@ class Solution:
         b = self.distance_x(root.right, x - 1)
         return a + b
 
+    # Problem No.909 Snakes and Ladders
+    #
+    # You are given an n x n integer matrix board where the cells are labeled from 1 to n^2 in a Boustrophedon style
+    # starting from the bottom left of the board (i.e. board[n - 1][0]) and alternating direction each row.
+    #
+    # You start on square 1 of the board. In each move, starting from square curr, do the following:
+    #
+    # Choose a destination square next with a label in the range [curr + 1, min(curr + 6, n^2)].
+    # This choice simulates the result of a standard 6-sided die roll: i.e., there are always at most 6 destinations,
+    # regardless of the size of the board.
+    # If next has a snake or ladder, you must move to the destination of that snake or ladder. Otherwise, you move to
+    # next.
+    # The game ends when you reach the square n2.
+    # A board square on row r and column c has a snake or ladder if board[r][c] != -1. The destination of that snake or
+    # ladder is board[r][c]. Squares 1 and n^2 do not have a snake or ladder.
+    #
+    # Note that you only take a snake or ladder at most once per move. If the destination to a snake or ladder is the
+    # start of another snake or ladder, you do not follow the subsequent snake or ladder.
+    #
+    # For example, suppose the board is [[-1,4],[-1,3]], and on the first move, your destination square is 2. You follow
+    # the ladder to square 3, but do not follow the subsequent ladder to 4.
+    # Return the least number of moves required to reach the square n2. If it is not possible to reach the square,
+    # return -1.
+    #
+    # Example 1:
+    #   Input: board = [
+    #   [-1,-1,-1,-1,-1,-1],
+    #   [-1,-1,-1,-1,-1,-1],
+    #   [-1,-1,-1,-1,-1,-1],
+    #   [-1,35,-1,-1,13,-1],
+    #   [-1,-1,-1,-1,-1,-1],
+    #   [-1,15,-1,-1,-1,-1]
+    #   ]
+    #   Output: 4
+    #   Explanation:
+    #   In the beginning, you start at square 1 (at row 5, column 0).
+    #   You decide to move to square 2 and must take the ladder to square 15.
+    #   You then decide to move to square 17 and must take the snake to square 13.
+    #   You then decide to move to square 14 and must take the ladder to square 35.
+    #   You then decide to move to square 36, ending the game.
+    #   This is the lowest possible number of moves to reach the last square, so return 4.
+    #
+    # Example 2:
+    #   Input: board = [[-1,-1],[-1,3]]
+    #   Output: 1
+    #
+    # Constraints:
+    #   n == board.length == board[i].length
+    #   2 <= n <= 20
+    #   grid[i][j] is either -1 or in the range [1, n2].
+    #   The squares labeled 1 and n2 do not have any ladders or snakes.
+    def snakesAndLadders(self, board: List[List[int]]) -> int:
+        n, num = len(board), len(board) * len(board)
+        end2start = [[] for _ in range(num)]
+        start2end = [-1] * num
+        for i in range(n):
+            for j in range(n):
+                index = (n - 1 - i) * n + (n - 1 - j if (n - 1 - i) % 2 else j)
+                if board[i][j] - 1 >= 0:
+                    end2start[board[i][j] - 1].append(index)
+                start2end[index] = board[i][j] - 1
+        waits = {num - 1}
+        visited = {num - 1}
+        step = 0
+        while len(waits) > 0:
+            step += 1
+            new_waits = set()
+            for end in waits:
+                if start2end[end] in visited or start2end[end] < 0:
+                    for i in range(end - 6, end):
+                        if i in visited:
+                            continue
+                        if i == 0:
+                            return step
+                        new_waits.add(i)
+                for perv in end2start[end]:
+                    for i in range(perv - 6, perv):
+                        if i in visited:
+                            continue
+                        if i == 0:
+                            return step
+                        new_waits.add(i)
+            print(step, new_waits)
+            visited.update(new_waits)
+            waits = new_waits
+        return -1
+
 
 if __name__ == '__main__':
-    print(Solution().makeIntegerBeautiful(16, 6))
-    print(Solution().makeIntegerBeautiful(467, 6))
-    print(Solution().makeIntegerBeautiful(1, 1))
-    print(Solution().makeIntegerBeautiful(846, 1))
-    print(Solution().makeIntegerBeautiful(590, 1))
-    print(Solution().makeIntegerBeautiful(213620191, 16))
-
-    # chars = [random.choice("abcdefghijklmnopqrstuvwxyz")] * 1000
-    # print(timeit.timeit(lambda: build1(chars), number=10000))
-    # print(timeit.timeit(lambda: build2(chars), number=10000))
-    # print(timeit.timeit(lambda: append1(), number=10000))
-    # print(timeit.timeit(lambda: append2(), number=10000))
-    # print(timeit.timeit(lambda: append3(), number=10000))
+    print(list(range(1, 6)))
