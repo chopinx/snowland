87fe81f288b62d5e0d21adc0e2fd1cbe710f4fcb
chopinxiao <chopinxiao@tencent.com>
2022-11-03 14:57:08 +0800

    feat: solved Problem No.973 K Closest Points to Origin

--- oct.py
+++ oct.py
@@ -1,4 +1,5 @@
 import heapq
+import random
 from collections import Counter
 from typing import List, Optional
 
@@ -1025,7 +1026,61 @@ class Solution:
                 return "{"
             else:
                 return item[gap + 1:] + "  " + item[:gap]
+
         return sorted(logs, key=get_key)
+
+    # Problem No.973 K Closest Points to Origin
+    #
+    # Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane and an integer k, return
+    # the k closest points to the origin (0, 0).
+    #
+    # The distance between two points on the X-Y plane is the Euclidean distance (i.e., √(x1 - x2)^2 + (y1 - y2)^2).
+    #
+    # You may return the answer in any order. The answer is guaranteed to be unique
+    # (except for the order that it is in).
+    #
+    # Example 1:
+    #   Input: points = [[1,3],[-2,2]], k = 1
+    #   Output: [[-2,2]]
+    #   Explanation:
+    #   The distance between (1, 3) and the origin is sqrt(10).
+    #   The distance between (-2, 2) and the origin is sqrt(8).
+    #   Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
+    #   We only want the closest k = 1 points from the origin, so the answer is just [[-2,2]].
+    #
+    # Example 2:
+    #   Input: points = [[3,3],[5,-1],[-2,4]], k = 2
+    #   Output: [[3,3],[-2,4]]
+    #   Explanation: The answer [[-2,4],[3,3]] would also be accepted.
+    #
+    # Constraints:
+    #   1 <= k <= points.length <= 10^4
+    #   -10^4 < xi, yi < 10^4
+
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        random.shuffle(points)
+        self.sort(points, 0, len(points) - 1, k)
+        return points[:k]
+
+    def get_dis(self, p):
+        return p[0] ** 2 + p[1] ** 2
+
+    def sort(self, points: list, start: int, end: int, k: int):
+        if start >= end:
+            return
+        mid_d = self.get_dis(points[start])
+        low, high = start + 1, end
+        while low <= high:
+            if self.get_dis(points[low]) <= mid_d:
+                low += 1
+            else:
+                points[low], points[high] = points[high], points[low]
+                high -= 1
+        points[start], points[high] = points[high], points[start]
+        if low < k:
+            self.sort(points, low, end, k)
+        elif low > k:
+            self.sort(points, start, low - 2, k)
 
 
 if __name__ == '__main__':
