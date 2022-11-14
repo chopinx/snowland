2a8fc4125328e0fdf449196ad01cfb2b81c0080a
chopinxiao <chopinxiao@tencent.com>
2022-11-14 16:20:11 +0800

    feat: sloved Problem No.2472 Maximum Number of Non-overlapping Palindrome Substrings

--- nov.py
+++ nov.py
@@ -280,6 +280,60 @@ class Solution:
             ans = max(ans, even)
         return ans
 
+    # Problem No.2472 Maximum Number of Non-overlapping Palindrome Substrings
+    #
+    # You are given a string s and a positive integer k.
+    #
+    # Select a set of non-overlapping substrings from the string s that satisfy the following conditions:
+    #
+    # The length of each substring is at least k.
+    # Each substring is a palindrome.
+    # Return the maximum number of substrings in an optimal selection.
+    #
+    # A substring is a contiguous sequence of characters within a string.
+    #
+    # Example 1:
+    #   Input: s = "abaccdbbd", k = 3
+    #   Output: 2
+    #   Explanation: We can select the substrings underlined in s = "abaccdbbd". Both "aba" and "dbbd" are palindromes
+    #   and have a length of at least k = 3.
+    #   It can be shown that we cannot find a selection with more than two valid substrings.
+    #
+    # Example 2:
+    #   Input: s = "adbcda", k = 2
+    #   Output: 0
+    #   Explanation: There is no palindrome substring of length at least 2 in the string.
+    #
+    # Constraints:
+    #   1 <= k <= s.length <= 2000
+    #   s consists of lowercase English letters.
+    def maxPalindromes(self, s: str, k: int) -> int:
+        if k == 1:
+            return len(s)
+        dp = [0] * (len(s) + 1)
+        letters = defaultdict(list)
+        for i in range(0, len(s)):
+            letters[s[i]].append(i)
+            if i < k - 1:
+                continue
+            dp[i] = dp[i - 1]
+            if len(letters[s[i]]) > 1:
+                for j in letters[s[i]][::-1]:
+                    if i - j + 1 < k:
+                        continue
+                    if dp[j - 1] + 1 <= dp[i]:
+                        break
+                    if self.isPalindrom(s[j:i + 1]):
+                        dp[i] = max(dp[i], dp[j - 1] + 1)
+                        break
+        return dp[-2]
+
+    @lru_cache(None)
+    def isPalindrom(self, s: str):
+        if len(s) == 1 or (len(s) == 2 and s[0] == s[1]):
+            return True
+        return s[0] == s[-1] and self.isPalindrom(s[1:-1])
+
 
 if __name__ == '__main__':
     a = [[i * 5 + j for j in range(5)] for i in range(5)]
