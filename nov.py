ac381d5e89e0def183dbdcc64d17bc656952834c
chopinxiao <chopinxiao@tencent.com>
2022-11-21 14:48:15 +0800

    feat: solved Problem No.2055 Plates Between Candles

--- nov.py
+++ nov.py
@@ -83,7 +83,7 @@ class TicTacToe:
     def __init__(self, n: int):
         self.n = n
         self.horizontal, self.vertical, self.diagonal = [
-            [0] * n, [0] * n], [[0] * n, [0] * n], [[0, 0], [0, 0]]
+                                                            [0] * n, [0] * n], [[0] * n, [0] * n], [[0, 0], [0, 0]]
 
     def move(self, row: int, col: int, player: int) -> int:
         cnt = self.horizontal[player -
@@ -552,6 +552,69 @@ class Solution:
         elif sum(cnt_0) == sum(cnt_1):
             return min(cnt_0)
         return -1
+
+    # Problem No.2055 Plates Between Candles
+    #
+    # There is a long table with a line of plates and candles arranged on top of it. You are given a 0-indexed string s
+    # consisting of characters '*' and '|' only, where a '*' represents a plate and a '|' represents a candle.
+    #
+    # You are also given a 0-indexed 2D integer array queries where queries[i] = [lefti, righti] denotes the substring
+    # s[lefti...righti] (inclusive). For each query, you need to find the number of plates between candles that are in
+    # the substring. A plate is considered between candles if there is at least one candle to its left and at least one
+    # candle to its right in the substring.
+    #
+    # For example, s = "||**||**|*", and a query [3, 8] denotes the substring "*||**|". The number of plates between
+    # candles in this substring is 2, as each of the two plates has at least one candle in the substring to its left
+    # and right.
+    # Return an integer array answer where answer[i] is the answer to the ith query.
+    #
+    # Example 1:
+    #   ex-1
+    #   Input: s = "**|**|***|", queries = [[2,5],[5,9]]
+    #   Output: [2,3]
+    #   Explanation:
+    #   - queries[0] has two plates between candles.
+    #   - queries[1] has three plates between candles.
+    #
+    # Example 2:
+    #   ex-2
+    #   Input: s = "***|**|*****|**||**|*", queries = [[1,17],[4,5],[14,17],[5,11],[15,16]]
+    #   Output: [9,0,0,0,0]
+    #   Explanation:
+    #   - queries[0] has nine plates between candles.
+    #   - The other queries have zero plates between candles.
+    #
+    # Constraints:
+    #   3 <= s.length <= 105
+    #   s consists of '*' and '|' characters.
+    #   1 <= queries.length <= 105
+    #   queries[i].length == 2
+    #   0 <= lefti <= righti < s.length
+    def platesBetweenCandles(self, s: str, queries: List[List[int]]) -> List[int]:
+        left_cnt = [0] * len(s)
+        plates_cnt = 0
+        first_p, last_p = [-1] * (len(s) + 1), [len(s)] * (len(s) + 1)
+        j = -1
+        for i, c in enumerate(s):
+            if c == '|':
+                left_cnt[i] = plates_cnt
+                last_p[i] = i
+                j += 1
+                while j < len(s) and s[j] != '|':
+                    j += 1
+                first_p[i] = j
+            else:
+                last_p[i] = last_p[i - 1]
+                first_p[i] = first_p[i - 1]
+                plates_cnt += 1
+        ans = []
+        for q in queries:
+            left, right = q[0], q[1]
+            if left <= first_p[left] < last_p[right] <= right:
+                ans.append(left_cnt[last_p[right]] - left_cnt[first_p[left]])
+            else:
+                ans.append(0)
+        return ans
 
 
 if __name__ == '__main__':
