9695a0333d795f7fd7434744eacd443123c17be8
chopinxiao <chopinxiao@tencent.com>
2022-11-22 10:59:27 +0800

    feat: solved Problem No.2262 Total Appeal of A String

--- nov.py
+++ nov.py
@@ -723,6 +723,48 @@ class Solution:
                 dp[2], dp[3], dp[5] = dp[2] + 1, dp[1] + dp[3], dp[4] + dp[5]
         return dp[5]
 
+    # Problem No.2262 Total Appeal of A String
+    #
+    # The appeal of a string is the number of distinct characters found in the string.
+    #
+    # For example, the appeal of "abbca" is 3 because it has 3 distinct characters: 'a', 'b', and 'c'.
+    # Given a string s, return the total appeal of all of its substrings.
+    #
+    # A substring is a contiguous sequence of characters within a string.
+    #
+    # Example 1:
+    #   Input: s = "abbca"
+    #   Output: 28
+    #   Explanation: The following are the substrings of "abbca":
+    #   - Substrings of length 1: "a", "b", "b", "c", "a" have an appeal of 1, 1, 1, 1, and 1 respectively.
+    # The sum is 5.
+    #   - Substrings of length 2: "ab", "bb", "bc", "ca" have an appeal of 2, 1, 2, and 2 respectively. The sum is 7.
+    #   - Substrings of length 3: "abb", "bbc", "bca" have an appeal of 2, 2, and 3 respectively. The sum is 7.
+    #   - Substrings of length 4: "abbc", "bbca" have an appeal of 3 and 3 respectively. The sum is 6.
+    #   - Substrings of length 5: "abbca" has an appeal of 3. The sum is 3.
+    #   The total sum is 5 + 7 + 7 + 6 + 3 = 28.
+    #
+    # Example 2:
+    #   Input: s = "code"
+    #   Output: 20
+    #   Explanation: The following are the substrings of "code":
+    #   - Substrings of length 1: "c", "o", "d", "e" have an appeal of 1, 1, 1, and 1 respectively. The sum is 4.
+    #   - Substrings of length 2: "co", "od", "de" have an appeal of 2, 2, and 2 respectively. The sum is 6.
+    #   - Substrings of length 3: "cod", "ode" have an appeal of 3 and 3 respectively. The sum is 6.
+    #   - Substrings of length 4: "code" has an appeal of 4. The sum is 4.
+    #   The total sum is 4 + 6 + 6 + 4 = 20.
+    #
+    # Constraints:
+    #   1 <= s.length <= 10^5
+    #   s consists of lowercase English letters.
+    def appealSum(self, s: str) -> int:
+        last_index, ans, sub_sum = {}, 0, 0
+        for i, c in enumerate(s):
+            sub_sum += i - last_index.get(c, -1)
+            last_index[c] = i
+            ans += sub_sum
+        return ans
+
 
 if __name__ == '__main__':
     size = 1000
